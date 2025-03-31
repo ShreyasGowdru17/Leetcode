0794a1a768eae5faaec9841e0d534f9ef8b0211b
Shreyas Gowda <Shreyaskgowda17@gmail.com>
2025-03-31 20:36:34 +0530

    Time: 0 ms (100%), Space: 17.7 MB (76.92%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.py
@@ -0,0 +1,39 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        def checkLive(a,b,m,n):
+            count_1=0
+            count_0=0
+            for i in range(a-1,a+2):
+                if i<0 or i>=m:
+                    continue
+                for j in range(b-1,b+2):
+                    if j<0 or j>=n or (i==a and j==b):
+                        continue
+                    if board[i][j]==1:
+                        count_1+=1
+                    elif board[i][j]==0:
+                        count_0+=1
+            return count_1,count_0
+        
+        m=len(board)
+        n=len(board[0])
+        counter=[]
+        for i in range(m):
+            for j in range(n):
+                count_1,count_0=checkLive(i,j,m,n)
+                if board[i][j]==1 and (count_1<2 or count_1>3):
+                    counter.append([i,j,0])
+                elif board[i][j]==0 and (count_1==3):
+                     counter.append([i,j,1])
+
+        for i,j,val in counter:
+            board[i][j]=val
+
+        
+
+
+            
+
