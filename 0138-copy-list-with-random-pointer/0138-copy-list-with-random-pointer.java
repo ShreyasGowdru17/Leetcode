18d94a0b055ae897e9f18931c9fdac7eb94665ad
Shreyas Gowda <Shreyaskgowda17@gmail.com>
2025-06-16 20:57:44 +0530

    Time: 0 ms (100%), Space: 44.5 MB (54.84%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,8 +15,40 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
+        // if(head==null){
+        //     return null;
+        // }
+        // Node cur=head;
+        // while(cur!=null){
+        //     Node node=new Node(cur.val);
+        //     Node temp=cur.next;
+        //     cur.next=node;
+        //     node.next=temp;
+        //     cur=cur.next.next;
+        // }
+
+        // cur=head;
+        // while(cur!=null){
+        //     if(cur.random!=null){
+        //         cur.next.random=cur.random.next;
+        //     }
+        //     cur=cur.next.next;
+        // }
+
+        // cur=head;
+        // Node cloneHead=head.next;
+        // Node clone=cloneHead;
+        // while(clone.next!=null){
+        //     cur.next=cur.next.next;
+        //     clone.next=clone.next.next;
+        //     cur=cur.next;
+        //     clone=clone.next;
+        // }
+        // cur.next=null;
+        // return cloneHead;
+
         if(head==null){
-            return null;
+            return head;
         }
         Node cur=head;
         while(cur!=null){
@@ -26,7 +58,6 @@ class Solution {
             node.next=temp;
             cur=cur.next.next;
         }
-
         cur=head;
         while(cur!=null){
             if(cur.random!=null){
@@ -34,17 +65,17 @@ class Solution {
             }
             cur=cur.next.next;
         }
-
         cur=head;
         Node cloneHead=head.next;
         Node clone=cloneHead;
         while(clone.next!=null){
             cur.next=cur.next.next;
             clone.next=clone.next.next;
-            cur=cur.next;
             clone=clone.next;
+            cur=cur.next;
         }
         cur.next=null;
         return cloneHead;
+        
     }
 }
