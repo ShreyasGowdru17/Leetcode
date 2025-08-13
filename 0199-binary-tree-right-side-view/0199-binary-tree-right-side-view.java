7fff043f784b8c7d3cada5720788f776305f62c6
Shreyas Gowda <Shreyaskgowda17@gmail.com>
2025-08-13 23:21:52 +0530

    Time: 2 ms (6.17%), Space: 42.4 MB (27.33%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -22,10 +22,16 @@ class Tuple{
     }
 }
 class Solution {
+    private void preorder(TreeNode node,int level,TreeMap<Integer,Integer> map){
+        if(node==null) return;
+        map.put(level,node.val);
+        preorder(node.left,level+1,map);
+        preorder(node.right,level+1,map);
+    }
     public List<Integer> rightSideView(TreeNode root) {
         if(root==null) return new ArrayList<>();
-        Map<Integer,Integer> map=new TreeMap<>();
-        Deque<Tuple> queue=new ArrayDeque<>();
+        TreeMap<Integer,Integer> map=new TreeMap<>();
+        /*Deque<Tuple> queue=new ArrayDeque<>();
         queue.offer(new Tuple(0,root));
         while(!queue.isEmpty()){
             Tuple tuple=queue.poll();
@@ -34,7 +40,8 @@ class Solution {
             map.put(level,cur.val);
             if(cur.left!=null) queue.offer(new Tuple(level+1,cur.left));
             if(cur.right!=null) queue.offer(new Tuple(level+1,cur.right));
-        }
+        }*/
+        preorder(root,0,map);
         List<Integer> result=new ArrayList<>();
         for(Integer value:map.values()){
             result.add(value);
