bda62589f3e424f1910de5420781ec6548f4267f
Shreyas Gowda <Shreyaskgowda17@gmail.com>
2025-08-13 22:57:38 +0530

    Time: 3 ms (2.92%), Space: 42.2 MB (63.36%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Tuple{
+    int level;
+    TreeNode node;
+    public Tuple(int level,TreeNode node){
+        this.level=level;
+        this.node=node;
+    }
+}
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        if(root==null) return new ArrayList<>();
+        Map<Integer,Integer> map=new TreeMap<>();
+        Deque<Tuple> queue=new ArrayDeque<>();
+        queue.offer(new Tuple(0,root));
+        while(!queue.isEmpty()){
+            Tuple tuple=queue.poll();
+            TreeNode cur=tuple.node;
+            int level=tuple.level;
+            map.put(level,cur.val);
+            if(cur.left!=null) queue.offer(new Tuple(level+1,cur.left));
+            if(cur.right!=null) queue.offer(new Tuple(level+1,cur.right));
+        }
+        List<Integer> result=new ArrayList<>();
+        for(Integer value:map.values()){
+            result.add(value);
+        }
+        return result;
+    }
+}
