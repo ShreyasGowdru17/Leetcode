9315988d931166922756be5043a48a2292e441a1
Shreyas Gowda <Shreyaskgowda17@gmail.com>
2025-12-03 23:27:59 +0530

    Time: 14 ms (76.08%), Space: 47.5 MB (15.79%) - LeetHub

---
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean winnerOfGame(String colors) {
+        int countA=0;
+        int countB=0;
+        int n=colors.length();
+        for(int i=1;i<n-1;i++){
+            if(colors.charAt(i)=='A' && colors.charAt(i-1)=='A' && colors.charAt(i+1)=='A') countA++;
+            else if(colors.charAt(i)=='B' && colors.charAt(i-1)=='B' && colors.charAt(i+1)=='B') countB++;
+        }
+        if(countA>countB) return true;
+        return false;
+    }
+}
