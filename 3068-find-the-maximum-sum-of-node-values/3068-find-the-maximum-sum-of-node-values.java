51a122639cd8b8b7992ab5c5a4a0eb3463db1065
Shreyas Gowda <Shreyaskgowda17@gmail.com>
2025-12-16 23:19:59 +0530

    Time: 1 ms (100%), Space: 54.5 MB (61.69%) - LeetHub

---
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long maximumValueSum(int[] nums, int k, int[][] edges) {
+        long sum=0;
+        int count=0;
+        int minAbsDiff=Integer.MAX_VALUE;
+        for(int num:nums){
+            if((num^k)>num){
+                count++;
+                sum+=num^k;
+            }else{
+                sum+=num;
+            }
+            minAbsDiff=Math.min(minAbsDiff,Math.abs(num-(num^k)));
+        }
+        if(count%2==0) return sum;
+        return sum-minAbsDiff;
+    }
+}
