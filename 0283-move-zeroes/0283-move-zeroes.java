eba6ce42e74481b8eb8f9bc262fa67a78dac5196
Shreyas Gowda <Shreyaskgowda17@gmail.com>
2026-01-08 22:18:48 +0530

    Time: 2 ms (89.35%), Space: 47.9 MB (38.75%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j=-1;
         int n=nums.length;
+        int j=-1;
         for(int i=0;i<n;i++){
             if(nums[i]==0){
                 j=i;
                 break;
             }
         }
-
         if(j==-1) return;
 
         for(int i=j+1;i<n;i++){
@@ -19,6 +18,5 @@ class Solution {
                 j++;
             }
         }
-        
     }
 }
