0dbbcf94edcc2b8d8a20547c10aa699016bd504c
Shreyas Gowda <Shreyaskgowda17@gmail.com>
2026-01-30 22:53:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -3,15 +3,15 @@ class Solution {
         int prefix=1;
         int suffix=0;
         int n=nums.length;
-        int maximum=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            if(prefix==0) prefix=1;
-            if(suffix==0) suffix=1;
-            prefix=prefix*nums[i];
-            suffix=suffix*nums[n-i-1];
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(prefix<=0) prefix=1;
+            if(suffix<=0) suffix=1;
+            prefix*=nums[i];
+            suffix*=nums[n-i-1];
             int total=Math.max(prefix,suffix);
-            maximum=Math.max(total,maximum);
+            max=Math.max(max,total);
         }
-        return maximum;
+        return max;
     }
 }
