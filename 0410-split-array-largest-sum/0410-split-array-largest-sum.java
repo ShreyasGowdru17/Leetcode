97ac39d26db6e4b4cdb4ec9e89ec9ee938c1b1ad
Shreyas Gowda <Shreyaskgowda17@gmail.com>
2026-02-14 15:28:15 +0530

    Time: 3 ms (14.43%), Space: 43.1 MB (50.44%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,33 +1,36 @@
 class Solution {
-    public int noOfSplits(int[] nums,int k,int mid){
-        int splits=1;
-        int largest=0;
-        for(int i=0;i<nums.length;i++){
-            if(largest+nums[i]<=mid){
-                largest+=nums[i];
-            }
-            else{
-                splits++;
-                largest=nums[i];
+    private boolean partition(int[] nums,int k,int maxValue){
+        int curSum = 0;
+        int parts=1;
+
+        for(int num : nums){
+            if(curSum + num <= maxValue){
+                curSum += num;
+            }else{
+                parts++;
+                curSum = num;
             }
         }
-        return splits;
+        return parts>k;
+
     }
     public int splitArray(int[] nums, int k) {
-        int left=Arrays.stream(nums).max().getAsInt();
-        int right=Arrays.stream(nums).sum();
-        int ans=Integer.MAX_VALUE;
-        while(left<=right){
-            int mid=(left+right)/2;
-            int splits=noOfSplits(nums,k,mid);
-            if(splits<=k){
-                ans=mid;
-                right=mid-1;
-            }
-            else{
-                left=mid+1;
+
+        int left = Arrays.stream(nums).max().getAsInt();
+        int right = Arrays.stream(nums).sum();
+
+        int ans = -1;
+
+        while(left <= right){
+
+            int mid = ( left + right)/2;
+            if(partition(nums,k,mid)){
+                left = mid + 1;
+            }else{
+                right = mid-1;
             }
         }
-        return ans;
+        
+        return left;
     }
 }
