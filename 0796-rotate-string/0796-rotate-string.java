661c45729d36ee0a934930162b0b63c4b03e50df
Shreyas Gowda <Shreyaskgowda17@gmail.com>
2026-02-20 21:53:20 +0530

    Time: 0 ms (100%), Space: 43 MB (41.83%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        int n1 = s.length();
+        int n2 = goal.length();
+        if(n1 != n2) return false;
+        String concat = s+s;
+        return concat.contains(goal);
+    }
+}
