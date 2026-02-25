f5a6c5c5fa40332203e508678bf235087482808b
Shreyas Gowda <Shreyaskgowda17@gmail.com>
2026-02-25 23:41:27 +0530

    Time: 1 ms (99.75%), Space: 44.1 MB (12.64%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,34 +1,31 @@
 class Solution {
     public int myAtoi(String s) {
-
-        s=s.trim();
-        if(s.length()==0) return 0;
-
-        int i=0;
-        int sign=1;
-        if(s.charAt(0)=='+'|| s.charAt(i)=='-'){
-            if(s.charAt(0)=='-'){
-                sign=sign*-1;
-            }
-            i++;
-        }
-
-        StringBuilder sb=new StringBuilder();
-        int n=s.length();
-        while(i<n && Character.isDigit(s.charAt(i))){
-            sb.append(s.charAt(i));
-            i++;
-        }
-
-        if(sb.length()==0) return 0;
-
-        try{
-
-            return sign*Integer.parseInt(sb.toString());
-        }
-        catch(Exception e){
-            return sign==1 ? Integer.MAX_VALUE:Integer.MIN_VALUE;
-        }
         
+        int sign = 1;
+        int i = 0;
+        int n = s.length();
+
+        while(i < n && s.charAt(i) == ' ') i++;
+
+        if (i == n ) return  0;
+        if(i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')){
+            sign = (s.charAt(i) == '-') ? -1 : 1 ;
+            i++;
+        }
+
+
+        int num = 0;
+        while( i < n && Character.isDigit(s.charAt(i))){
+
+                int digit = s.charAt(i)-'0';
+                if (num > Integer.MAX_VALUE / 10 ||
+               (num == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+                num = num * 10 + digit;
+                i++;
+        }
+
+       return sign * num ;
     }
 }
