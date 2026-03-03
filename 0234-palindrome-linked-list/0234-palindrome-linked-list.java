e9bebce65ac51d23f5f95ea3df950d086e8aef3d
Shreyas Gowda <Shreyaskgowda17@gmail.com>
2026-03-03 20:58:30 +0530

    Time: 5 ms (47.13%), Space: 94.5 MB (67.55%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,42 +10,53 @@
  */
 class Solution {
     public ListNode reverse(ListNode head){
-        if(head.next==null){
+        if(head.next == null){
             return head;
         }
-        ListNode cur=head;
-        ListNode prev=null;
-        while(cur!=null){
-            ListNode temp=cur.next;
-            cur.next=prev;
-            prev=cur;
-            cur=temp;
+
+        ListNode prev = null;
+        ListNode cur = head;
+
+        while(cur != null){
+            ListNode nextNode = cur.next; 
+            cur.next = prev;
+            prev = cur;
+            cur = nextNode;
         }
-        return prev;
+
+        return prev; 
     }
     public boolean isPalindrome(ListNode head) {
-        if(head==null || head.next==null){
-            return true;
-        }
-        ListNode slow=head;
-        ListNode fast=head;
-        while(fast.next!=null && fast.next.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        if(fast!=null){
-            slow=slow.next;
-        }
-        ListNode newHead=reverse(slow);
-        ListNode first=head;
-        ListNode second=newHead;
-        while(second!=null){
-            if (second.val!=first.val){
+
+      if(head == null || head.next == null){
+         return true;
+      }   
+
+      ListNode slow = head;
+      ListNode fast = head;
+
+      while(fast.next != null && fast.next.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+      }
+
+      if(fast != null){
+        slow = slow.next;
+      }
+
+      ListNode newHead = reverse(slow);
+      ListNode first = head;
+      ListNode second = newHead;
+
+      while(second != null){
+
+            if(first.val != second.val){
                 return false;
             }
-            second=second.next;
-            first=first.next;
-        }
-        return true;
+            first = first.next;
+            second = second.next;
+      }
+
+      return true;
     }
 }
